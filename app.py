bb7fa293281af1b37b89c0452b155c1e87b1056f
amirfitri128 <amirfitri128@gmail.com>
2025-07-10 14:51:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 st.title("read CV from Github")
 
-url = "https://raw.githubusercontent.com/amirfitri128/main/House_Price_Dataset.csv"
+url = "https://raw.githubusercontent.com/amirfitri128/tobi_price/main/House_Price_Dataset.csv"
 
 def load_data():
   return pd.read_csv(url)
