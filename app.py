6f855f6010b3c27029d2e73ac38847bd1f28a3aa
amirfitri128 <amirfitri128@gmail.com>
2025-07-10 14:42:38 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as it
+import pandas as pd
+import base64
+import request
+
+st.title("read CV from Github")
+
+url = "https://raw.githubusercontent.com/limfw/temp/main/data.csv"
+
+def load_data():
+  return pd.read_csv(url)
+
+try:
+  df = load_data()
+  st.success("Data loaded successfully!")
+  st.dataframe(df)
+
+except Exception as e:
+  ste.error(f"Failed to load data: {e})
