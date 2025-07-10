ed468baac4aeb86821fdd7bf528160a0a385750f
amirfitri128 <amirfitri128@gmail.com>
2025-07-10 15:31:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,15 @@ try:
 
 except Exception as e:
   st.error("Failed to load data: {e}")
+
+">>https://github.com/settings/personal-access-tokens "
+a = st.text_input("Enter a")
+b = st.text_input("Enter b")
+c = st.text_input("Enter c")
+
+if st.button("Uploadnto Github"):
+  df = pd.DataFrame([{"a": a, "b": b, "c": c}])
+  csv = df.to_csv(index=False)
+  content = base64.b64encode(csv.encode()).decode()
+
+url = "https://api.github.com/repos/amirfitri128/tobi-price/data2.csv"
